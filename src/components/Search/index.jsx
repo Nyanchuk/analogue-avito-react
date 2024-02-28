650af5898e52371d75b37d5ecaa9327eccde9b3c
global_gifts <fifa.1998.sava@mail.ru>
2024-02-28 13:36:26 +1000

    поиск работает

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,14 +1,20 @@
 import styles from './styles.module.css';
 import logo from '../../img/main_img/Logo.svg';
 
-const Search = () => {
-    return (
+const Search = ({ onSearchInputChange, onSearchButtonClick, searchTerm, onShowAllClick}) => {
+  return (
     <div className={styles.main_search}>
-        <img src={logo} alt='logo'/>
-        <input className={styles.main_block_search} placeholder="Поиск по объявлениям"></input>
-        <button className={styles.main_block_search_button}>Найти</button>
-      </div>
-    );
+      <img src={logo} alt='logo'/>
+      <input 
+        className={styles.main_block_search} 
+        placeholder="Поиск по объявлениям"
+        value={searchTerm}
+        onChange={onSearchInputChange}
+      />
+      <button className={styles.main_block_search_button} onClick={onSearchButtonClick}>Найти</button>
+      <button className={styles.main_block_return_button} onClick={onShowAllClick} >Все объявления</button>
+    </div>
+  );
 }
 
 export default Search;
--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -6,16 +6,21 @@ import { useEffect, useState } from 'react';
 import { getAllAds } from '../../api';
 
 export const Main = () => {
+  // Храним оригинальный массив объявлений
   const [products, setProducts] = useState([]);
-  // Стейт для хранения ошибок
+  // Храним отфильтрованный массив объявлений
+  const [filterProducts, setFilterProducts] = useState([])
+  // Для ошибок
   const [error, setError] = useState('');
+  // Для поиска 
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const data = await getAllAds();
         setProducts(data);
-        console.log(data)
+        setFilterProducts(data);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -24,25 +29,46 @@ export const Main = () => {
     fetchData();
   }, []);
   // Функция для обновления страницы после добавления нового объявления
-const fetchAndUpdateProducts = async () => {
-  try {
-    const data = await getAllAds();
-    setProducts(data);
-    console.log(data)
-  } catch (error) {
-    console.error(error);
-    setError('Произошла ошибка: ' + error.message);
-  }
-};
+  const fetchAndUpdateProducts = async () => {
+    try {
+      const data = await getAllAds();
+      setProducts(data);
+      setFilterProducts(data);
+    } catch (error) {
+      console.error(error);
+      setError('Произошла ошибка: ' + error.message);
+    }
+  };
+  // Обработчик изменения строки поиска
+  const handleSearchInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+  // Фильтрация
+  const handleSearchButtonClick = () => {
+    const filtered = products.filter(product =>
+      product.title.toLowerCase().charAt(0) === searchTerm.toLowerCase()
+    );
+    setFilterProducts(filtered);
+  };
+  // Сброс фильтров
+  const handleShowAllButtonClick = () => {
+    setFilterProducts(products);
+    setSearchTerm('');
+  };
   return (
     <div className={styles.container}>
       <Header  onAddNewAd={fetchAndUpdateProducts} />
       <div className={styles.conteiners}>
-        <Search />
+      <Search 
+          onSearchInputChange={handleSearchInputChange} 
+          onSearchButtonClick={handleSearchButtonClick} 
+          searchTerm={searchTerm} 
+          onShowAllClick={handleShowAllButtonClick}
+        />
         <div className={styles.main__container}>
           <div className={styles.main__h2}>Объявления</div>
           <div className={styles.main__content}>
-            {products.map(product => (
+            {filterProducts.map(product => (
               <ProductCard key={product.id} product={product} />
             ))}
           </div>
